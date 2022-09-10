c36c8039d01b8f8484a2b0ea48d908ff9d43543a
Adewole Muhammed <adewoleadetunji77@gmail.com>
2022-09-10 10:48:07 +0100

    dodov page

---
+++ blog.js
@@ -0,0 +1,13 @@
+
+const setTheme = theme => document.documentElement.className = theme;
+
+document.getElementById('theme-select').addEventListener('change', function() {
+    setTheme(this.value);
+});
+
+const getTheme = () =>{
+    const theme = localStorage.getItem('theme');
+    theme && setTheme(theme);
+}
+
+getTheme();
